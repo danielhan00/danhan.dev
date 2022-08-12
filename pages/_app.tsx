59d445e917232a44335a79522509a62425ec951b
danielhan60903 <danielhan60903@gmail.com>
2022-08-12 12:53:58 -0700

    chore: update ts files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import '../styles/global.css'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
