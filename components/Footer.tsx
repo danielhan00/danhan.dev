e589d72a755f2bfc3c092945db6f375e460415e8
danielhan60903 <danielhan60903@gmail.com>
2022-08-17 10:47:02 -0700

    feat: header formatting

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export interface FooterProps {
 export const Footer = (props) => {
   return (
     <footer className="mt-10 justify-center gap-2 text-center text-sm print:hidden md:mt-20">
-      <p className="text-green">Built by Daniel Han :)</p>
+      <p className="text-green">Built by Daniel Han :) WIP as of 8/16/22</p>
     </footer>
   );
 };
