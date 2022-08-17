e78e4eaa22d6042b86a4b6076b8e2ef6868e2380
danielhan60903 <danielhan60903@gmail.com>
2022-08-17 15:08:43 -0700

    feat: adding links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,33 @@ export const Footer = (props) => {
   return (
     <footer className="bg-peach w-full flex-wrap items-center justify-between py-4 border-4 font-con pt-8 pb-8 ">
       <p className="text-center">
-        Built by Daniel Han :) <br />
-        inspired by fellow Northeastern University Alum Brittany Chiang
+        <a
+          href="https://github.com/danielhan00/danhan.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-green"
+        >
+          Built{" "}
+        </a>
+        by Daniel Han :) <br />
+        inspired by fellow{" "}
+        <a
+          href="https://www.khoury.northeastern.edu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-green"
+        >
+          Northeastern University{" "}
+        </a>
+        Alum{" "}
+        <a
+          href="https://brittanychiang.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-green"
+        >
+          Brittany Chiang
+        </a>
       </p>
     </footer>
   );
